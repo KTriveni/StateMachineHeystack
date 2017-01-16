9514f971f95e1ec61a3272d06e22b28d87787e2d
KTriveni <triveni.employment@gmail.com>
2017-01-16 22:14:06 +0530

    state machine tool tips

--- app/models/state_machine.rb
+++ app/models/state_machine.rb
@@ -2,5 +2,5 @@ class StateMachine < ActiveRecord::Base
   has_attached_file :photo, :styles => { :small => "150x150>" , :original => "500x500>", :large => "800x800>" }
   do_not_validate_attachment_file_type :photo
   has_many :state_machine_nodes , :dependent => :destroy
-  has_many :state_machine_resource_nodes 
+  has_many :state_machine_resource_nodes , :dependent => :destroy
 end
