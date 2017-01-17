9d48b329dda7903e8d4bb6828c3f16f26e548a9c
KTriveni <triveni.employment@gmail.com>
2017-01-17 09:50:17 +0530

    fixing journey

--- app/helpers/state_machine_nodes_helper.rb
+++ app/helpers/state_machine_nodes_helper.rb
@@ -1,14 +1,18 @@
 module StateMachineNodesHelper
 	def record_title(record)
-		user_journeys = []
+		@user_journeys = []
 		record.each do |journey|
 			user_journey = []
 			node_ids = journey.split(',')
       node_ids.each do |node_id| 
 	      user_journey << StateMachineNode.find_by(state_machine_id: params[:id],state_node_id: node_id.strip).node_title
       end
-      user_journeys <<  user_journey
+      @user_journeys <<  user_journey
 		end
-		return user_journeys.uniq
+		return @user_journeys.uniq
+	end
+
+	def record_count(record)
+		record_title(record).count
 	end
 end
