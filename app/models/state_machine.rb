db58b4d5dc769bcd81588c62a91dd33f6a826315
KTriveni <triveni.employment@gmail.com>
2017-01-19 01:53:25 +0530

    final fix for journey

--- app/models/state_machine.rb
+++ app/models/state_machine.rb
@@ -1,5 +1,5 @@
 class StateMachine < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :small => "150x150>" , :original => "500x500>", :large => "800x800>" }
+  has_attached_file :photo, :styles => { :small => "150x150>" ,:ultra_large => "2000x2000>", :original => "500x500>", :large => "800x800>" }
   do_not_validate_attachment_file_type :photo
   has_many :state_machine_nodes , :dependent => :destroy
   has_many :state_machine_resource_nodes 
---
+++ test/models/state_machine_node_resource_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StateMachineNodeResourceTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
